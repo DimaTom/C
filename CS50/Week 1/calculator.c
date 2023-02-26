9674a4f2b3921762e78d8508d2f82fb6a12bdc62
Dmytro Tomniuk <dmitrijtomnuk@Dmytros-MacBook-Pro.local>
2023-02-26 23:51:27 +0100

    1st version

--- CS50/Week 1/calculator.c
+++ CS50/Week 1/calculator.c
@@ -6,6 +6,6 @@ int main(void)
     long x = get_long("x: ");
     long y = get_long("y: ");
 
-    double z = (double) x/ (double) y;
+    double z = (double) x / (double) y;
     printf("%.20f\n", z);
 }
